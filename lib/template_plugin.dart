2db3537598d3b285d65322f715a9e79e417b8166
hazard <softshoes67@gmail.com>
2024-05-27 22:30:54 -0500

    Attempting to fix plugin system

--- lib/template_plugin.dart
+++ lib/template_plugin.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+import 'package:template_plugin/models/plugin_models.dart';
+
+class MyPlugin extends Plugin {
+  @override
+  void initialize(ExtensionContext context) {
+    context.showMessage('Hello from MyPlugin!');
+  }
+}
+
+Plugin main() {
+  return MyPlugin();
 }
